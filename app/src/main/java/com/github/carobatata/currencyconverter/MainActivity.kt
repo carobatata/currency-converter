64c9a2f24772fd29c2d81596555cd60f20e830a3
carolinaacosta <aacostacarolina@gmail.com>
2025-12-01 18:48:25 +0100

    feat: finishing first screen

--- app/src/main/java/com/github/carobatata/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/github/carobatata/currencyconverter/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -12,16 +11,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import com.github.carobatata.currencyconverter.ui.CurrencyConverterScreen
 import com.github.carobatata.currencyconverter.ui.theme.CurrencyConverterTheme
 import com.github.carobatata.currencyconverter.ui.theme.app_blue
 import com.github.carobatata.currencyconverter.ui.theme.app_light_blue
-import com.github.carobatata.currencyconverter.ui.theme.regular_padding
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -47,31 +43,13 @@ class MainActivity : ComponentActivity() {
                         )
                     },
                 ) { innerPadding ->
-                    Column(
+                    CurrencyConverterScreen(
                         modifier = Modifier
                             .padding(innerPadding)
-                            .padding(regular_padding)
-                    ) {
-                        CurrencyConverterScreen()
-                    }
+                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CurrencyConverterTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/github/carobatata/currencyconverter/ui/CurrencyConverterScreen.kt
+++ app/src/main/java/com/github/carobatata/currencyconverter/ui/CurrencyConverterScreen.kt
@@ -3,19 +3,47 @@ package com.github.carobatata.currencyconverter.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.SwapVerticalCircle
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.carobatata.currencyconverter.ui.theme.app_blue
 import com.github.carobatata.currencyconverter.ui.theme.regular_padding
 
 @Preview
 @Composable
-fun CurrencyConverterScreen() {
+fun CurrencyConverterScreen(modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier
+            .padding(regular_padding)
+            .fillMaxSize()
     ) {
+        CurrencyCard()
+        HorizontalDivider(
+            modifier = Modifier.padding(regular_padding),
+            color = Color.Transparent,
+        )
+        AmountField()
+        HorizontalDivider(
+            modifier = Modifier.padding(regular_padding),
+            color = Color.Transparent,
+        )
+        Icon(
+            imageVector = Icons.Filled.SwapVerticalCircle,
+            modifier = Modifier.size(48.dp),
+            contentDescription = null,
+            tint = app_blue
+        )
+        HorizontalDivider(
+            modifier = Modifier.padding(regular_padding),
+            color = Color.Transparent,
+        )
         CurrencyCard()
         HorizontalDivider(
             modifier = Modifier.padding(regular_padding),
